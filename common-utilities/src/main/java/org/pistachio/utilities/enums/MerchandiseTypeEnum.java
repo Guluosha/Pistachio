5ff6f0a1d34e67b02a0f91c1724f89bdbe68018b
单曲循环 <1019844337@qq.com>
2019-01-29 16:23:36 +0800

    【add】: 1.新增商品类型枚举类

---
+++ common-utilities/src/main/java/org/pistachio/utilities/enums/MerchandiseTypeEnum.java
@@ -0,0 +1,14 @@
+package org.pistachio.utilities.enums;
+
+/**
+ * CopyRight (C),深圳市万古盛世互联科技有限公司
+ * <br/><b/>
+ * 商品类型枚举
+ *
+ * @author SingleCycle(QQ ： 单曲循环)
+ * @date 2019/1/29 ~ 下午 4:22
+ */
+
+public enum MerchandiseTypeEnum {
+
+}
