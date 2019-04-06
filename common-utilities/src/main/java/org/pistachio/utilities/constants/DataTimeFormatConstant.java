76b7bb3dd4345337e9eb16b0534de57e5362b982
单曲循环 <1019844337@qq.com>
2019-04-06 18:34:14 +0800

    【add】：1.添加日期时间格式常量类

---
+++ common-utilities/src/main/java/org/pistachio/utilities/constants/DataTimeFormatConstant.java
@@ -0,0 +1,34 @@
+package org.pistachio.utilities.constants;
+
+/**
+ * CopyRight (C),深圳市万古盛世互联科技有限公司
+ * <br/><b/>
+ * 日期时间格式常量
+ *
+ * @author SingleCycle(QQ ： 单曲循环)
+ * @date 2019/4/6 ~ 下午 6:27
+ */
+
+public interface DataTimeFormatConstant {
+
+    /**
+     * 年月日（yyyy-MM-dd）
+     */
+    String YYYY_MM_DD = "yyyy-MM-dd";
+
+    /**
+     * 时分秒（hh:mm:ss）
+     */
+    String HH_MM_SS = "HH:mm:ss";
+
+    /**
+     * 年月日 时分秒（yyyy-MM-dd HH:mm:ss）
+     */
+    String YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd HH:mm:ss";
+
+    /**
+     * 年月日 时分秒毫秒（yyyy-MM-dd HH:mm:ss:SSS）
+     */
+    String YYYY_MM_DD_HH_MM_SS_SSS = "yyyy-MM-dd HH:mm:ss:SSS";
+
+}
