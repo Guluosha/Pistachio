5f4c5a1df280739b7defe5bda76a6e7a53a1a2d5
单曲循环 <1019844337@qq.com>
2019-04-06 18:35:42 +0800

    【add】：1.添加自定义异常包装类（抽象异常基类）

---
+++ common-utilities/src/main/java/org/pistachio/utilities/exception/AbstractExceptionWrapper.java
@@ -0,0 +1,49 @@
+package org.pistachio.utilities.exception;
+
+import com.netflix.zuul.exception.ZuulException;
+
+/**
+ * CopyRight (C),深圳市万古盛世互联科技有限公司
+ * <br/><b/>
+ * 自定义异常消息包装类
+ *
+ * @author SingleCycle(QQ ： 单曲循环)
+ * @date 2019/3/23 ~ 上午 11:52
+ */
+
+public abstract class AbstractExceptionWrapper extends ZuulException {
+
+    /**
+     * Source Throwable, message, status code and info about the cause
+     *
+     * @param throwable   异常基类
+     * @param sMessage    异常信息
+     * @param nStatusCode 异常码
+     * @param errorCause  异常原因
+     */
+    public AbstractExceptionWrapper(Throwable throwable, String sMessage, int nStatusCode, String errorCause) {
+        super(throwable, sMessage, nStatusCode, errorCause);
+    }
+
+    /**
+     * error message, status code and info about the cause
+     *
+     * @param sMessage    异常信息
+     * @param nStatusCode 异常码
+     * @param errorCause  异常原因
+     */
+    public AbstractExceptionWrapper(String sMessage, int nStatusCode, String errorCause) {
+        super(sMessage, nStatusCode, errorCause);
+    }
+
+    /**
+     * Source Throwable,  status code and info about the cause
+     *
+     * @param throwable   异常基类
+     * @param nStatusCode 异常码
+     * @param errorCause  异常原因
+     */
+    public AbstractExceptionWrapper(Throwable throwable, int nStatusCode, String errorCause) {
+        super(throwable, nStatusCode, errorCause);
+    }
+}
