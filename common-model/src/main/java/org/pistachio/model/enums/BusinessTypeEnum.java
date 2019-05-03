dea4c142af09e0ff703601660de38310010f7062
单曲循环 <1019844337@qq.com>
2019-05-04 00:21:18 +0800

    【add】：1.添加业务类型枚举类

--- common-model/src/main/java/org/pistachio/model/enums/BusinessTypeEnum.java
+++ common-model/src/main/java/org/pistachio/model/enums/BusinessTypeEnum.java
@@ -3,10 +3,41 @@ package org.pistachio.model.enums;
 /**
  * CopyRight (C),深圳市万古盛世互联科技有限公司
  * <br/><b/>
+ * 业务枚举类
  *
  * @author SingleCycle(QQ ： 单曲循环)
  * @date 2019/5/3 ~ 下午 11:57
  */
 
 public enum BusinessTypeEnum {
+    ;
+
+    private Integer businessTypeSerialNumber;
+
+    BusinessTypeEnum(Integer businessTypeSerialNumber) {
+        this.businessTypeSerialNumber = businessTypeSerialNumber;
+    }
+
+    public Integer getBusinessTypeSerialNumber() {
+        return businessTypeSerialNumber;
+    }
+
+    /**
+     * 根据业务类型值获取业务类型枚举值
+     *
+     * @param businessTypeSerialNumber 请求参数
+     * @return BusinessTypeEnum，业务类型枚举
+     */
+    public static BusinessTypeEnum getBusinessTypeEnumBySerialNumber(Integer businessTypeSerialNumber) {
+        if (businessTypeSerialNumber != null) {
+            for (BusinessTypeEnum businessTypeEnum : values()) {
+                if (businessTypeEnum.businessTypeSerialNumber.equals(businessTypeSerialNumber)) {
+                    return businessTypeEnum;
+                }
+            }
+            return null;
+        } else {
+            return null;
+        }
+    }
 }
