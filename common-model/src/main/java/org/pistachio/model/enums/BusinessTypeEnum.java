f442e04928e7f17a619c21efbc640780975022d0
单曲循环 <1019844337@qq.com>
2019-05-04 00:01:27 +0800

    【add】：1.添加common-model项目模块；2.修改项目结构

---
+++ common-model/src/main/java/org/pistachio/model/enums/BusinessTypeEnum.java
@@ -0,0 +1,12 @@
+package org.pistachio.model.enums;
+
+/**
+ * CopyRight (C),深圳市万古盛世互联科技有限公司
+ * <br/><b/>
+ *
+ * @author SingleCycle(QQ ： 单曲循环)
+ * @date 2019/5/3 ~ 下午 11:57
+ */
+
+public enum BusinessTypeEnum {
+}
