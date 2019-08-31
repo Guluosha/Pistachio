7a103fe68bf830a5f64e6f74419bcc283377df44
单曲循环 <1019844337@qq.com>
2019-08-31 12:39:21 +0800

    【other】：1.添加线程相关问题求证代码

---
+++ common-utilities/src/main/java/org/pistachio/utilities/context/SpringApplicationContextHolder.java
@@ -0,0 +1,12 @@
+package org.pistachio.utilities.context;
+
+/**
+ * CopyRight (C),深圳市万古盛世互联科技有限公司
+ * <br/><b/>
+ *
+ * @author SingleCycle(QQ ： 单曲循环)
+ * @date 2019/8/9 ~ 下午 11:53
+ */
+
+public class SpringApplicationContextHolder {
+}
