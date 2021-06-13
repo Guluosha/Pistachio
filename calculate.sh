aa7d9d60209fe40c7d71029e678f804394935234
单曲循环 <1019844337@qq.com>
2021-06-13 15:12:42 +0800

    【other】：1.网关部分修改

--- calculate.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-inputArray=(8 3 9 5 10 2)
-bubbleSort() {
-  for ((marginIndex = 0; marginIndex < ${#inputArray[*]}; marginIndex++)); do
-    echo "数字：${inputArray[marginIndex]}"
-  done
-}
-bubbleSort
