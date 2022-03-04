9f76a1db9b13d170086b51c09fad83430d90c8c7
单曲循环 <1019844337@qq.com>
2022-03-04 23:19:32 +0800

    【other】：1.添加公共常量类

---
+++ common-utilities/src/main/java/org/pistachio/utilities/constants/DateTimeFormatConstant.java
@@ -0,0 +1,34 @@
+package org.pistachio.utilities.constants;
+
+/**
+ * CopyRight C;深圳市壹账通智能科技有限公司
+ * <br/><b/>
+ *
+ * @author SingleCycle
+ * @version 日期时间格式常量
+ * @date 2022/03/04 ~ 下午 7:26
+ * @since 1.0.0
+ */
+
+public interface DateTimeFormatConstant {
+
+    /**
+     * “年-月-日”
+     */
+    String YYYY_MM_DD = "yyyy-MM-dd";
+
+    /**
+     * “时-分-秒”
+     */
+    String HH_MM_SS = "HH:mm:ss";
+
+    /**
+     * “年-月-日 时-分-秒”
+     */
+    String YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd HH:mm:ss";
+
+    /**
+     * “年-月-日 时-分-秒-毫秒”
+     */
+    String YYYY_MM_DD_HH_MM_SS_SSS = "yyyy-MM-dd HH:mm:ss:SSS";
+}
---
+++ common-utilities/src/main/java/org/pistachio/utilities/constants/SymbolConstant.java
@@ -0,0 +1,64 @@
+package org.pistachio.utilities.constants;
+
+/**
+ * CopyRight (C),深圳市壹账通智能科技有限公司
+ * <br/><b/>
+ *
+ * @author SingleCycle
+ * @version 符号常量
+ * @date 2022/03/04 ~ 下午 7:29
+ * @since 1.0.0
+ */
+
+public interface SymbolConstant {
+
+    /**
+     * 斜杠
+     */
+    String SLASH = "/";
+
+    /**
+     * 反斜杠
+     */
+    String BACK_SLASH = "\\";
+
+    /**
+     * 逗号
+     */
+    String COMMA = ",";
+
+    /**
+     * 分号
+     */
+    String SEMICOLON = ";";
+
+    /**
+     * 冒号
+     */
+    String COLON = ":";
+
+    /**
+     * 下划线
+     */
+    String UNDER_LINE = "_";
+
+    /**
+     * 加号
+     */
+    String PLUS = "+";
+
+    /**
+     * 减号（连字符）
+     */
+    String HYPHEN = "-";
+
+    /**
+     * 问号
+     */
+    String QUESTION_MARK = "?";
+
+    /**
+     * 管道符
+     */
+    String PIPE_SYMBOL = "|";
+}
